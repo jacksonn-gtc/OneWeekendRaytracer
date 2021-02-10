8a04b2a29240df3b9c6c7507cf933518c70206ba
jacksonn <jacksonn@users.noreply.github.com>
2021-02-09 23:29:08 -0800

    updated world.hit() min, sped way up; section 8.23

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ color ray_color(const ray& r, const hittable& world, int depth) {
     if (depth <= 0)
         return color(0, 0, 0);
 
-    if(world.hit(r, 0, infinity, rec)) {
+    if(world.hit(r, 0.001, infinity, rec)) {
         point3 target = rec.p + rec.normal + random_in_unit_sphere();       // pick random point in unit sphere centered at rec.p + rec.normal
         return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1); // fire a new ray to the point
     }
