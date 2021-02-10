7d6245d9aca67143d791202a37d096b00314f206
jacksonn <jacksonn@users.noreply.github.com>
2021-02-09 18:45:03 -0800

    update hittable.h hit_record, sphere.h sphere::hit, section 6.14

--- hittable.h
+++ hittable.h
@@ -7,6 +7,12 @@ struct hit_record {
     point3 p;
     vec3 normal;
     double t;
+    bool front_face;
+
+    inline void set_face_normal(const ray& r, const vec3& outward_normal) {
+        front_face = dot(r.direction(), outward_normal) < 0;    // determine if ray came from outside (dot<0) or inside (dot>0)
+        normal = front_face ? outward_normal : -outward_normal; // make new normal always point against ray
+    }
 };
 
 class hittable {
