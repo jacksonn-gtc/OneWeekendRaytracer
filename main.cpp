e62f9643a6009525b1c71a02684f2cd257d3dda8
jacksonn <jacksonn@users.noreply.github.com>
2021-02-09 17:24:22 -0800

    initial commit, section 6.13

---
+++ main.cpp
@@ -0,0 +1,59 @@
+#include "vec3.h"
+#include "color.h"
+#include "ray.h"
+#include "hittable.h"
+#include "sphere.h"
+
+#include <iostream>
+
+hit_record rec;
+sphere s = sphere(point3(0,0,-1), 0.5);
+
+color ray_color(const ray& r) {
+    if(s.hit(r, 0, 1, rec)) {
+        vec3 N = unit_vector(r.at(rec.t) - vec3(0,0,-1)); // r.at(t): hit point
+        return 0.5*color(N.x()+1, N.y()+1, N.z()+1);
+    }
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5*(unit_direction.y() + 1.0);
+    return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
+}
+
+int main() {
+
+    // Image
+
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
+
+    // Render
+
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for(int j=image_height-1; j>=0; --j) {
+        std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+        for(int i=0; i<image_width; ++i) {
+            auto u = double(i) / (image_width-1);
+            auto v = double(j) / (image_height-1);
+            ray r(origin, lower_left_corner + u*horizontal + v*vertical - origin);
+            color pixel_color = ray_color(r);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    std::cerr << "\nDone.\n";
+
+    return 0;
+}
