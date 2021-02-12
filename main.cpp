e10333bebeb964110652dc4e0b8fb5ef0a79a4d4
jacksonn <jacksonn@users.noreply.github.com>
2021-02-12 03:27:34 -0800

    added adjustable camera fov; section 11.37

--- main.cpp
+++ main.cpp
@@ -40,20 +40,17 @@ int main() {
 
     // World
 
+    auto R = cos(pi/4);
     hittable_list world;
 
-    auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<dielectric>(1.5);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+    auto material_left   = make_shared<lambertian>(color(0,0,1));
+    auto material_right  = make_shared<lambertian>(color(1,0,0));
 
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100, material_ground));  // place the ground sphere first, else it obscures previous objects
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0), 0.5, material_right));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0), 0.5, material_center));  // place the center sphere last, so it obscures the others
+    world.add(make_shared<sphere>(point3(-R, 0.0, -1.0), R, material_left));
+    world.add(make_shared<sphere>(point3( R, 0.0, -1.0), R, material_right));
     
     // Camera
-    camera cam;
+    camera cam(60.0, aspect_ratio);
 
     // Render
 
