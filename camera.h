41f5b7dd22f1a367fa1711e159e4ff3d88ea1513
jacksonn <jacksonn@sfu.a>
2021-02-13 02:10:46 -0800

    added defocus blur/ depth of field; recreated book cover; A Final Render, section 13.41

--- camera.h
+++ camera.h
@@ -10,7 +10,9 @@ class camera {
             point3 lookat,
             vec3   vup,
             double vfov, // vertical field-of-view in degrees
-            double aspect_ratio
+            double aspect_ratio,
+            double aperture,
+            double focus_dist
         ) {
             auto theta = degrees_to_radians(vfov);
             auto h = tan(theta/2);
@@ -19,22 +21,30 @@ class camera {
 
             //std::cerr << "viewport_height:" << viewport_height << ", viewport_width:" << viewport_width << '\n';
             
-            auto w = unit_vector(lookfrom - lookat);
-            auto u = unit_vector(cross(vup, w));
-            auto v = cross(w, u);
+            w = unit_vector(lookfrom - lookat);
+            u = unit_vector(cross(vup, w));
+            v = cross(w, u);
             
             //std::cerr << "w:" << w << ", u:" << u << ", v:" << v <<'\n';
             
             origin = lookfrom;
-            horizontal = viewport_width * u;
-            vertical = viewport_height * v;
-            lower_left_corner = origin - horizontal/2 - vertical/2 - w;
+            horizontal = focus_dist * viewport_width * u;
+            vertical = focus_dist * viewport_height * v;
+            lower_left_corner = origin - horizontal/2 - vertical/2 - focus_dist*w;
 
             //std::cerr << "origin:" << origin << ", horiz:" << horizontal/2 << ", vert:" << vertical/2 << ", low_left:" << lower_left_corner << '\n';
+
+            lens_radius = aperture / 2;
         }
 
         ray get_ray(double s, double t) const {
-            return ray(origin, lower_left_corner + s*horizontal + t*vertical - origin);
+            vec3 rd = lens_radius * random_in_unit_disk();  // pick a random point for our sample to start from
+            vec3 offset = u * rd.x() + v * rd.y();
+
+            return ray(
+                origin + offset, 
+                lower_left_corner + s*horizontal + t*vertical - origin - offset
+            );
         }
     
     public:
@@ -42,6 +52,8 @@ class camera {
         vec3 horizontal;
         vec3 vertical;
         point3 lower_left_corner;
+        vec3 w,u,v;
+        double lens_radius;
 };
 
 #endif
